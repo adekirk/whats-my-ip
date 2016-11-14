a19de693a841221e2aea5aa9d266c8a4bb43816e
Adrian Kirk <code@adekirk.co.uk>
2016-11-14 17:38:49 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# whats-my-ip
-Simple public IP API service.
+# What's My IP
+A really simple IP address API
+
+
+## API Usage
+
+Only two options plain or JSON:
+
+| URI | Response Type | Example |
+------|---------------|---------|
+http://whats-my-ip.azurewebsites.net | text | 127.0.0.1 
+http://whats-my-ip.azurewebsites.net?format=json | json | {"ip":"127.0.0.1"}
+
+
+## Examples
+
+### Terminal
+```
+$ curl 'http://whats-my-ip.azurewebsites.net?format=json'
+{"ip":"127.0.0.1"}
+```
---
+++ Source/WhatsMyIp/Controllers/IpController.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+
+namespace WhatsMyIp.Controllers
+{
+    public class IpController : ApiController
+    {
+        [Route("")]
+        public HttpResponseMessage Get([FromUri] string format = "")
+        {
+            var ip = GetClientIp();
+            StringContent content = null;
+
+            switch (format?.ToLowerInvariant())
+            {
+                default:
+                    content = new StringContent($"{ip}", Encoding.ASCII, "text/plain");
+                    break;
+
+                case "json":
+                    content = new StringContent($"{{\"ip\":\"{ip}\"}}", Encoding.ASCII, "application/json");
+                    break;
+            }
+
+
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = content
+            };
+        }
+
+        private string GetClientIp()
+        {
+            return HttpContext.Current?.Request.UserHostAddress;
+        }
+    }
+}
